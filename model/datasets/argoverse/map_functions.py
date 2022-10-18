Work on fixing wrong values after social encoding

model/datasets/argoverse/map_functions.py
@@ -271,26 +271,6 @@ class MapFeaturesUtils:
         ]
         return sorted_lane_seqs, sorted_scores
 
-    def sort_lanes_based_on_point_in_polygon_score_custom(self, centerlines,xy_seq,city_name,avm):
-        """
-        """
-
-        lane_seq_polygon = [Polygon(centerline) for centerline in centerlines]
-
-        polygons = list(lane_seq_polygon)
-
-        list_contains = []
-
-        for polygon in polygons:
-            point_in_polygon_score = 0
-            for xy in xy_seq:
-                if polygon.contains(Point(xy)):
-                    point_in_polygon_score += 1
-            list_contains.append(point_in_polygon_score)
-
-        print("list_contains: ", list_contains)
-        pdb.set_trace()
-
     def get_heuristic_centerlines_for_test_set(
             self,
             lane_seqs: List[List[int]],
@@ -366,14 +346,18 @@ class MapFeaturesUtils:
         # https://en.wikipedia.org/wiki/Speed_limits_in_the_United_States_by_jurisdiction
         # 1 miles per hour (mph) ~= 1.609 kilometers per hour (kph)
         # Common speed limits in the USA (in highways): 70 - 80 mph -> (112 kph - 129 kph) -> (31.29 m/s - 35.76 m/s)
-        # The average is around 120 kph -> 33.33 m/s, so if the vehicle is accelerating strongly (in the observation has reached the 
-        # maximum velocity), we assume the GT will be without acceleration (e.g. Seq 188893 in train). The maximum prediction horizon
-        # should be around 100 m.
+        # The average is around 120 kph -> 33.33 m/s, so if the vehicle has accelerated strongly (in the last observation has reached the 
+        # maximum velocity), we assume the GT will be without acceleration, that is, braking or stop accelerating positively 
+        # (e.g. Seq 188893 in train). 
+        # The maximum prediction horizon should be around 100 m.
 
         #                            | (Limit of observation)
         #                            v
-        # ... . . .  .  .  .    .    .    .     .     .      .      .      .      .      .      .      .  
-        # [     Observation data     ][                       Groundtruth data                         ]
+        # ... . . .  .  .  .    .    .    .    .    .     .     .     .     .     .     .     .     . (Good interpretation)
+
+        # [     Observation data     ][                       Groundtruth data                      ]  
+
+        # ... . . .  .  .  .    .    .    .     .     .      .       .       .       .       .        .        .          . (Wrong interpretation) 
 
         x = agent_seq[:,0]
         y = agent_seq[:,1]
@@ -855,7 +839,7 @@ class MapFeaturesUtils:
             viz: bool = False,
             max_search_radius: float = 50.0,
             seq_len: int = 50,
-            max_candidates: int = 10,
+            max_candidates: int = 6,
             mode: str = "test",
             split: str = "train",
             algorithm: str = "competition", # competition, map_api, get_around
@@ -901,10 +885,10 @@ class MapFeaturesUtils:
 
         vel, acc, xy_filtered, extended_xy_filtered = self.get_agent_velocity_and_acceleration(xy,
                                                                                                filter="least_squares",
-                                                                                               debug=True)
+                                                                                               debug=False)
                                                                                                
         dist_around = vel * (pred_len/freq) + 1/2 * acc * (pred_len/freq)**2
-        # print("dist around: ", dist_around)
+
         if dist_around < min_dist_around:
             dist_around = min_dist_around
 
@@ -1053,7 +1037,10 @@ class MapFeaturesUtils:
                 distances.append(min(np.linalg.norm((centerline - reference_point),axis=1)))
             
             unique_distances = list(set(distances))
+            unique_distances.sort()
+            unique_distances = unique_distances[:max_candidates]
 
+            # print("unique distances: ", unique_distances)
             final_indeces = [np.where(distances == unique_distance)[0][0] for unique_distance in unique_distances]
 
             final_candidates = []
@@ -1100,10 +1087,6 @@ class MapFeaturesUtils:
                     lane_ids.append(lane_id)
             candidate_centerlines = lane_centerlines
 
-            
-            agent_traj = np.dot(R,agent_traj.T).T
-            agent_traj -= last_obs
-
             ### Filter centerline which start point is in front of the agent last observation
 
             # yaw_aux = math.pi/2 - yaw # In order to align the data with the vertical axis
@@ -1149,7 +1132,7 @@ class MapFeaturesUtils:
                 plt.ylim(ymin, ymax)
                 plt.axis("off")
 
-            # Paint agent's orientation
+            ## Paint agent's orientation
 
             # dx = lane_dir_vector[0] * 10
             # dy = lane_dir_vector[1] * 10
@@ -1164,9 +1147,9 @@ class MapFeaturesUtils:
             #     zorder=16,
             # )
 
-            if algorithm == "map_api":
-                filename = os.path.join(output_dir,f"{seq_id}_binary_plausible_area.png")
-                plt.savefig(filename, bbox_inches='tight', facecolor=fig.get_facecolor(), edgecolor='none', pad_inches=0)
+            # if algorithm == "map_api":
+            #     filename = os.path.join(output_dir,f"{seq_id}_binary_plausible_area.png")
+            #     plt.savefig(filename, bbox_inches='tight', facecolor=fig.get_facecolor(), edgecolor='none', pad_inches=0)
 
             if agent_traj.shape[0] == seq_len: # train and val
                 # Agent observation
@@ -1272,7 +1255,7 @@ class MapFeaturesUtils:
                 filename = os.path.join(output_dir,f"{seq_id}_relevant_centerlines.png")
             else:
                 filename = os.path.join(output_dir,f"{seq_id}_oracle.png")
-
+            print("filename: ", filename)
             plt.savefig(filename, bbox_inches='tight', facecolor="white", edgecolor='none', pad_inches=0)
 
             plt.show()
@@ -1368,7 +1351,6 @@ class MapFeaturesUtils:
             oracle_nt_dist = get_nt_distance(agent_xy,
                                              oracle_centerline,
                                              viz=viz)
-        print("Candidates: ", len(candidate_centerlines))
 
         map_feature_helpers = {
             "ORACLE_CENTERLINE": oracle_centerline,

model/models/sophie_mm.py
@@ -24,11 +24,12 @@ NUM_MODES = 6
 OBS_LEN = 20
 PRED_LEN = 30
 NUM_PLAUSIBLE_AREA_POINTS = 512
+CENTERLINE_LENGTH = 30 # Starting from the last observation
 
 MLP_DIM = 64
 H_DIM = 128
 EMBEDDING_DIM = 16
-CONV_FILTERS = 60 # 60
+CONV_FILTERS = 8 # 60
 
 APPLY_DROPOUT = True
 DROPOUT = 0.4
@@ -233,6 +234,9 @@ class Multimodal_DecoderLSTM(nn.Module):
                 pred.append(Linear(self.h_dim, self.data_dim*self.pred_len))
             self.hidden2pos = nn.ModuleList(pred) 
 
+        self.ln1 = nn.LayerNorm(self.data_dim*self.num_modes)
+        self.ln2 = nn.LayerNorm(self.h_dim)
+
         self.confidences = nn.Linear(self.h_dim, self.num_modes)
 
     def forward(self, last_obs, last_obs_rel, state_tuple):
@@ -249,7 +253,9 @@ class Multimodal_DecoderLSTM(nn.Module):
         last_obs_rel = last_obs_rel.view(1, batch_size, -1)
 
         pred_traj_fake_rel = []
-        decoder_input = F.leaky_relu(self.spatial_embedding(last_obs_rel.contiguous().view(batch_size, -1))) 
+        # decoder_input = F.leaky_relu(self.spatial_embedding(last_obs_rel.contiguous().view(batch_size, -1))) 
+        decoder_input = F.tanh(self.spatial_embedding(self.ln1(last_obs_rel.contiguous().view(batch_size, -1)))) 
+        self.ln1
         if APPLY_DROPOUT: decoder_input = F.dropout(decoder_input, p=DROPOUT, training=self.training)
         decoder_input = decoder_input.contiguous().view(1, batch_size, self.embedding_dim)
 
@@ -281,9 +287,11 @@ class Multimodal_DecoderLSTM(nn.Module):
 
                     rel_pos = torch.cat(rel_pos,dim=1)   
                 elif HEAD == "SingleLinear":
-                    rel_pos = self.hidden2pos(state_tuple_h_.contiguous().view(-1, self.h_dim))
+                    rel_pos = self.hidden2pos(self.ln2(state_tuple_h_.contiguous().view(-1, self.h_dim)))
 
-                decoder_input = F.leaky_relu(self.spatial_embedding(rel_pos.contiguous().view(batch_size, -1)))
+                # decoder_input = F.leaky_relu(self.spatial_embedding(rel_pos.contiguous().view(batch_size, -1)))
+                decoder_input = F.tanh(self.spatial_embedding(self.ln1(rel_pos.contiguous().view(batch_size, -1)))) 
+
                 if APPLY_DROPOUT: decoder_input = F.dropout(decoder_input, p=DROPOUT, training=self.training)
                 decoder_input = decoder_input.contiguous().view(1, batch_size, self.embedding_dim)           
                 pred_traj_fake_rel.append(rel_pos.contiguous().view(batch_size,-1))
@@ -418,7 +426,7 @@ class Centerline_Encoder(nn.Module):
         self.data_dim = DATA_DIM
         self.num_filters = CONV_FILTERS
         self.kernel_size = 3
-        self.lane_length = 40
+        self.lane_length = CENTERLINE_LENGTH
 
         self.pooling_size = 2
 
@@ -453,6 +461,7 @@ class Centerline_Encoder(nn.Module):
         self.linear = nn.Linear(self.num_filters*aux_length,self.h_dim)
         self.bn = nn.BatchNorm1d(self.h_dim)
         self.tanh = torch.nn.Tanh()
+        self.leaky_relu = nn.LeakyReLU(0.1)
 
     def forward(self, phy_info):
         """
@@ -460,18 +469,39 @@ class Centerline_Encoder(nn.Module):
 
         batch_size = phy_info.shape[0]
 
-        phy_info_ = self.conv1(phy_info.permute(0,2,1))
-        phy_info_ = self.maxpooling1(phy_info_)
-        phy_info_ = self.conv2(phy_info_)
-        phy_info_ = self.maxpooling2(phy_info_)
-        phy_info_ = self.conv3(phy_info_)
+        # phy_info_ = self.conv1(phy_info.permute(0,2,1))
+        # phy_info_ = self.maxpooling1(phy_info_)
+        # phy_info_ = self.conv2(phy_info_)
+        # phy_info_ = self.maxpooling2(phy_info_)
+        # phy_info_ = self.conv3(phy_info_)
 
-        phy_info_ = self.linear(phy_info_.view(batch_size,-1))
-        phy_info_ = self.bn(phy_info_)
-        phy_info_ = self.tanh(phy_info_)
-        phy_info_ = F.dropout(phy_info_, p=DROPOUT, training=self.training)
+        # phy_info_ = self.linear(phy_info_.view(batch_size,-1))
+        # phy_info_ = self.bn(phy_info_)
+        # phy_info_ = self.tanh(phy_info_)
+        # phy_info_ = F.dropout(phy_info_, p=DROPOUT, training=self.training)
 
-        return phy_info_
+        # if torch.any(phy_info_.isnan()):
+        #     pdb.set_trace()
+
+        # return phy_info_
+
+        phy_info_a = self.conv1(phy_info.permute(0,2,1))
+        phy_info_b = self.maxpooling1(phy_info_a)
+        phy_info_b = self.leaky_relu(phy_info_b)
+        phy_info_c = self.conv2(phy_info_b)
+        phy_info_d = self.maxpooling2(phy_info_c)
+        phy_info_d = self.leaky_relu(phy_info_d)
+        phy_info_e = self.conv3(phy_info_d)
+
+        phy_info_1 = self.linear(phy_info_e.view(batch_size,-1))
+        phy_info_2 = self.bn(phy_info_1)
+        phy_info_2 = self.leaky_relu(phy_info_2)
+        phy_info_3 = F.dropout(phy_info_2, p=DROPOUT, training=self.training)
+
+        if torch.any(phy_info_3.isnan()):
+            pdb.set_trace()
+
+        return phy_info_3
         
 class TrajectoryGenerator(nn.Module):
     def __init__(self, PHYSICAL_CONTEXT="dummy"):
@@ -540,6 +570,9 @@ class TrajectoryGenerator(nn.Module):
 
         encoded_social_info = torch.cat(out_self_attention,dim=0) # batch_size · num_agents x num_inputs_decoder · hidden_dim
 
+        if torch.any(encoded_social_info.isnan()):
+            pdb.set_trace()
+
         if np.any(agent_idx): # single agent
             encoded_social_info = encoded_social_info[agent_idx,:]
  
@@ -568,6 +601,7 @@ class TrajectoryGenerator(nn.Module):
         
         elif self.physical_context == "oracle":
             encoded_phy_info = self.centerline_encoder(phy_info)
+
             mlp_decoder_context_input = torch.cat([encoded_social_info.contiguous().view(-1,self.h_dim),
                                                    encoded_phy_info.contiguous().view(-1,self.h_dim)],
                                                    dim=1)

preprocess/preprocess_data.py
@@ -64,8 +64,8 @@ config.dataset.start_from_percentage = 0.0
 
 # Preprocess data
                          # Split, Process, Split percentage
-splits_to_process = dict({"train":[True,1.0], # 0.01 (1 %), 0.1 (10 %), 1.0 (100 %)
-                          "val":  [False,0.01],
+splits_to_process = dict({"train":[False,0.01], # 0.01 (1 %), 0.1 (10 %), 1.0 (100 %)
+                          "val":  [True,0.01],
                           "test": [False,1.0]})
 modes_centerlines = ["test"] # "train","test" 
 # if train -> compute the best candidate (oracle), only using the "competition" algorithm
@@ -79,12 +79,12 @@ pred_len = 30 # steps
 freq = 10 # Hz ("steps/s")
 obs_origin = 20 
 min_dist_around = 25
-max_points = 40
+max_points = pred_len # In order to match with the number of future steps in Argoverse
 min_points = 4 # to perform a cubic interpolation you need at least 3 points
+algorithm = "map_api"
 
 viz = False
 limit_qualitative_results = 150
-debug = False
 check_every = 0.1 # % of total files
 
 RAW_DATA_FORMAT = {
@@ -178,8 +178,7 @@ for split_name,features in splits_to_process.items():
             for i, file_id in enumerate(file_id_list):
                 # print(f"File {file_id} -> {i+1}/{len(file_id_list)}")
 
-                # if file_id != -1:
-                if file_id == 188893:
+                if file_id != -1:
 
                     if limit_qualitative_results != -1 and i+1 > limit_qualitative_results:
                         viz = False
@@ -201,9 +200,20 @@ for split_name,features in splits_to_process.items():
                     first_obs = agent_xy[0,:]
                     last_obs = agent_xy[obs_origin-1,:]
 
-                    # TODO: This is wrong -> Compute the orientation using the filtered observation, not 
-                    # the raw data
-                    lane_dir_vector, yaw = map_features_utils_instance.get_yaw(agent_xy, obs_len)
+                    # Filter agent's trajectory (smooth)
+
+                    vel, acc, xy_filtered, extended_xy_filtered = map_features_utils_instance.get_agent_velocity_and_acceleration(agent_xy[:obs_len,:],
+                                                                                                                                  filter="least_squares",
+                                                                                                                                  debug=False)
+                                                                                                        
+                    dist_around = vel * (pred_len/freq) + 1/2 * acc * (pred_len/freq)**2
+
+                    if dist_around < min_dist_around:
+                        dist_around = min_dist_around
+
+                    # Compute agent's orientation
+
+                    lane_dir_vector, yaw = map_features_utils_instance.get_yaw(xy_filtered, obs_len)
 
                     for mode in modes_centerlines:
                         # Map features extraction
@@ -218,7 +228,7 @@ for split_name,features in splits_to_process.items():
                                 mode,
                                 avm,
                                 viz,
-                                algorithm="map_api" # competition, map_api, get_around
+                                algorithm=algorithm # competition, map_api, get_around
                             )
 
                         if mode == "test": # preprocess N plausible centerlines
@@ -226,26 +236,19 @@ for split_name,features in splits_to_process.items():
                             start_ = time.time()
 
                             relevant_centerlines = map_feature_helpers["CANDIDATE_CENTERLINES"]  
-                            pdb.set_trace()
+
                             seq_map_info = dict()
                             relevant_centerlines_filtered = []                 
 
-                            for relevant_centerline in relevant_centerlines:
+                            for index_centerline, relevant_centerline in enumerate(relevant_centerlines):
                                 # Get index of the closest waypoint to the first observation
                                 
-                                closest_wp_first, _ = map_features_utils_instance.get_closest_wp(first_obs, relevant_centerline)
+                                closest_wp_first, _ = map_features_utils_instance.get_closest_wp(last_obs, relevant_centerline)
 
                                 # Get index of the closest waypoint to the last observation + dist_around
                             
                                 closest_wp_last, dist_array_last = map_features_utils_instance.get_closest_wp(last_obs, relevant_centerline)
                                 dist_array_last = dist_array_last[closest_wp_last:] # To determine dist around from this point
-
-                                # TODO: The acceleration is quite noisy. Improve this calculation
-                                vel, acc = map_features_utils_instance.get_agent_velocity_and_acceleration(agent_xy[:obs_len,:],
-                                                                                                        filter="least_squares")                         
-                                dist_around = vel * (pred_len/freq) + 1/2 * acc * (pred_len/freq)**2
-                                if dist_around < min_dist_around:
-                                    dist_around = min_dist_around
 
                                 idx, value = find_nearest(dist_array_last,dist_around)
                                 num_points = idx + (closest_wp_last - closest_wp_first)
@@ -337,6 +340,14 @@ for split_name,features in splits_to_process.items():
                                         assert interpolated_centerline.shape[0] == max_points
 
                                         relevant_centerlines_filtered.append(interpolated_centerline)
+
+                                        # Store oracle
+
+                                        if index_centerline == 0 and algorithm == "map_api": 
+                                            # The first centerline also corresponds to the oracle using map_api
+
+                                            oracle_centerlines_list.append(interpolated_centerline)
+
                                     except:
                                         # TODO: Take the closest max_points if the previous algorithm fails
                                         
@@ -347,6 +358,13 @@ for split_name,features in splits_to_process.items():
                                 else:
                                     # print("The algorithm has the exact number of points")
                                     relevant_centerlines_filtered.append(relevant_centerline_filtered)            
+
+                                    # Store oracle
+
+                                    if index_centerline == 0 and algorithm == "map_api": 
+                                        # The first centerline also corresponds to the oracle using map_api
+
+                                        oracle_centerlines_list.append(relevant_centerline_filtered)
 
                             seq_map_info["relevant_centerlines_filtered"] = relevant_centerlines_filtered
 
@@ -393,14 +411,14 @@ for split_name,features in splits_to_process.items():
                             plt.ylim(ymin, ymax)
                             plt.axis("off")
 
-                            filename = os.path.join(output_dir,f"{file_id}_binary_plausible_area_filtered.png")
+                            filename = os.path.join(output_dir,f"{file_id}_binary_plausible_area_filtered_gray.png")
                             # plt.savefig(filename, bbox_inches='tight', facecolor=fig.get_facecolor(), edgecolor='none', pad_inches=0)
                             fig.tight_layout(pad=0)
                             fig.canvas.draw()
                             img_gray = cv2.cvtColor(np.asarray(fig.canvas.buffer_rgba()), cv2.COLOR_RGBA2GRAY)
 
                             cv2.imwrite(filename, img_gray)
-                            plt.close('all')
+                            # plt.close('all')
 
                             plt.arrow(
                                 agent_xy[obs_len-1,0], # Arrow origin
@@ -479,8 +497,17 @@ for split_name,features in splits_to_process.items():
                                     zorder=15,
                                 )
 
+                            filename = os.path.join(output_dir,f"{file_id}_binary_plausible_area_filtered_color.png")
                             # plt.savefig(filename, bbox_inches='tight', facecolor=fig.get_facecolor(), edgecolor='none', pad_inches=0)
-                            # plt.close('all')
+
+                            lane_polygon = centerline_to_polygon(relevant_centerlines_filtered[0])
+                            ax.fill(lane_polygon[:, 0], lane_polygon[:, 1], "green", edgecolor='green', fill=True)
+                            fig.tight_layout(pad=0)
+                            fig.canvas.draw()
+                            img_bgr = cv2.cvtColor(np.asarray(fig.canvas.buffer_rgba()), cv2.COLOR_RGBA2BGR)
+
+                            cv2.imwrite(filename, img_bgr)
+                            plt.close('all')
 
                             # plt.xlabel("Map X")
                             # plt.ylabel("Map Y")
@@ -493,7 +520,7 @@ for split_name,features in splits_to_process.items():
                             # print("Time consumed: ", end_-start_)
 
                             map_info[str(file_id)] = seq_map_info 
-                        
+
                         elif mode == "train": # only best centerline ("oracle")
                             start_ = time.time()
 
@@ -507,13 +534,6 @@ for split_name,features in splits_to_process.items():
                             
                             closest_wp_last, dist_array_last = map_features_utils_instance.get_closest_wp(last_obs, oracle_centerline)
                             dist_array_last = dist_array_last[closest_wp_last:] # To determine dist around from this point
-
-                            # TODO: The acceleration is quite noisy. Improve this calculation
-                            vel, acc = map_features_utils_instance.get_agent_velocity_and_acceleration(agent_xy[:obs_len,:],
-                                                                                                    filter="least_squares")                         
-                            dist_around = vel * (pred_len/freq) + 1/2 * acc * (pred_len/freq)**2
-                            if dist_around < min_dist_around:
-                                dist_around = min_dist_around
 
                             idx, value = find_nearest(dist_array_last,dist_around)
                             num_points = idx + (closest_wp_last - closest_wp_first)
@@ -599,7 +619,7 @@ for split_name,features in splits_to_process.items():
                                                                                                                     viz=viz)
                                     # end = time.time()
                                     # print("Time consumed by interpolation: ", end-start)
-                                    pdb.set_trace()
+
                                     assert interpolated_centerline.shape[0] == max_points
 
                                     oracle_centerlines_list.append(interpolated_centerline)
@@ -627,14 +647,12 @@ for split_name,features in splits_to_process.items():
                                 Estimated time to finish ({files_remaining} files): {round(time_per_iteration*files_remaining/60)} min")
                         print("Wrong centerlines: ", wrong_centerlines) 
 
+            # TODO: At this moment, both train and test use the same algorithm
+
             # Save only the oracle (best possible centerline) as a np.array -> num_sequences x max_points x 2 
             if mode == "train":
-                if debug:
-                    filename = os.path.join(BASE_DIR,config.dataset.path,split_name,
-                                            f"data_processed_{str(int(features[1]*100))}_percent","debug_oracle_centerlines.npy")
-                else:
-                    filename = os.path.join(BASE_DIR,config.dataset.path,split_name,
-                                            f"data_processed_{str(int(features[1]*100))}_percent","oracle_centerlines.npy")
+                filename = os.path.join(BASE_DIR,config.dataset.path,split_name,
+                                        f"data_processed_{str(int(features[1]*100))}_percent","oracle_centerlines.npy")
                 with open(filename, 'wb') as my_file: np.save(my_file, oracle_centerlines_list)
 
             # Save N centerlines per sequence. Note that the number of variables per sequence may vary
@@ -642,5 +660,15 @@ for split_name,features in splits_to_process.items():
                 filename = os.path.join(BASE_DIR,config.dataset.path,split_name,
                                         f"data_processed_{str(int(features[1]*100))}_percent","relevant_centerlines.npz")
                 with open(filename, 'wb') as my_file: np.savez(my_file, map_info)
+
+                # If method is least_squares and map_api, we assume the first centerline returned by the 
+                # algorithm will be also the oracle (most plausible)
+
+                if algorithm == "map_api":
+                    filename = os.path.join(BASE_DIR,config.dataset.path,split_name,
+                                        f"data_processed_{str(int(features[1]*100))}_percent","oracle_centerlines.npy")
+                    oracle_centerlines_array = np.array(oracle_centerlines_list)
+                    with open(filename, 'wb') as my_file: np.save(my_file, oracle_centerlines_array)
+
             
             