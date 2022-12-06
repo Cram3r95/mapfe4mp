Fix some bugs with centerline rotation

evaluate/argoverse/generate_results_rel-rel.py
@@ -30,8 +30,10 @@ from prodict import Prodict
 
 # DL & Math imports
 
+import math
 import numpy as np
 import torch
+
 from torch.utils.data import DataLoader
 
 # Custom imports
@@ -42,6 +44,7 @@ sys.path.append(BASE_DIR)
 
 import model.datasets.argoverse.plot_functions as plot_functions
 import model.datasets.argoverse.dataset_utils as dataset_utils
+import model.datasets.argoverse.data_augmentation_functions as data_augmentation_functions
 
 from model.datasets.argoverse.dataset import ArgoverseMotionForecastingDataset, seq_collate
 from model.utils.checkpoint_data import get_generator
@@ -71,7 +74,7 @@ ARGOVERSE_NUM_MODES = 6
 dist_around = 40
 dist_rasterized_map = [-dist_around, dist_around, -dist_around, dist_around]
 
-GENERATE_QUALITATIVE_RESULTS = False
+GENERATE_QUALITATIVE_RESULTS = True
 PLOT_WORST_SCENES = False
 LIMIT_QUALITATIVE_RESULTS = 150
 
@@ -293,7 +296,6 @@ def evaluate(loader, generator, config, split, current_cuda, pred_len, results_p
 
                     pred_traj_fake_rel, conf = generator(obs_traj, obs_traj_rel, seq_start_end, agent_idx, phy_info=plausible_area, relevant_centerlines=relevant_centerlines)
                 else:
-                    # pred_traj_fake_rel, conf = generator(obs_traj, obs_traj_rel, seq_start_end, agent_idx, phy_info=phy_info, relevant_centerlines=relevant_centerlines)
                     pred_traj_fake_rel, conf = generator(obs_traj, obs_traj_rel, seq_start_end, agent_idx, relevant_centerlines=relevant_centerlines)
                     
                 ## Get predictions in absolute -> map coordinates
@@ -308,7 +310,32 @@ def evaluate(loader, generator, config, split, current_cuda, pred_len, results_p
             # same forward)
 
             ade_min, fde_min = None, None
+            
+            # Transform to CPU (to acelerate the rotation and plot qualitative results)
+            
+            obs_traj = obs_traj.cpu()
+            pred_traj_gt = pred_traj_gt.cpu()
+            pred_traj_fake = pred_traj_fake.cpu()
+            map_origin = map_origin.cpu()
+            relevant_centerlines = relevant_centerlines.cpu()
+            target_agent_orientation = target_agent_orientation.cpu()
+            non_linear_obj = non_linear_obj.cpu()
+            object_cls = object_cls.cpu()
+            
+            if config.dataset.apply_rotation: # We have to counter-rotate in order to have again the original sequence
+                yaw_aux = (math.pi/2 - target_agent_orientation) # Apply this angle to align the trajectory with the Y-axis
+                c, s = torch.cos(yaw_aux), torch.sin(yaw_aux)
+                R = torch.tensor([[c,-s],  # Rot around the map z-axis
+                                [s, c]])
 
+                obs_traj = data_augmentation_functions.rotate_traj(obs_traj,R)
+                pred_traj_gt = data_augmentation_functions.rotate_traj(pred_traj_gt,R)
+                pred_traj_fake = data_augmentation_functions.rotate_traj(pred_traj_fake,R)
+                map_origin = data_augmentation_functions.rotate_traj(map_origin,R)
+                relevant_centerlines = data_augmentation_functions.rotate_traj(relevant_centerlines,R)
+
+                pred_traj_fake_global = pred_traj_fake + map_origin
+                
             if COMPUTE_METRICS and split != "test":
                 agent_pred_gt = pred_traj_gt[:,agent_idx,:] # pred_len (30) x 1 (agent) x data_dim (2) -> "Abs" coordinates (around 0,0)
                 agent_pred_fake = pred_traj_fake
@@ -318,8 +345,8 @@ def evaluate(loader, generator, config, split, current_cuda, pred_len, results_p
 
                 agent_obj_id = obj_id[agent_idx]
                 agent_mask = np.where(agent_obj_id.cpu() == -1, 0, 1)
-                agent_mask = torch.tensor(agent_mask, device=agent_obj_id.device).reshape(-1)
-                
+                # agent_mask = torch.tensor(agent_mask, device=agent_obj_id.device).reshape(-1)
+
                 ade, ade_min = cal_ade_multimodal(agent_pred_gt, agent_pred_fake, agent_linear_obj, agent_non_linear_obj, agent_mask)
                 fde, fde_min = cal_fde_multimodal(agent_pred_gt, agent_pred_fake, agent_linear_obj, agent_non_linear_obj, agent_mask)
 
@@ -342,11 +369,11 @@ def evaluate(loader, generator, config, split, current_cuda, pred_len, results_p
                 plot_scene += 1
                 # Custom plot
 
+                curr_map_origin = map_origin[0]
+                
                 if split == "test":
-                    curr_map_origin = map_origin[0]
                     curr_traj = obs_traj
                 else:
-                    curr_map_origin = map_origin[0][0] # TODO: Generate again val and train data without [[]] in the map!
                     curr_traj = torch.cat((obs_traj,
                                            pred_traj_gt),dim=0)
 
@@ -354,28 +381,23 @@ def evaluate(loader, generator, config, split, current_cuda, pred_len, results_p
 
                 # Argoverse standard plot
 
-                if config.hyperparameters.physical_context == "plausible_centerlines+area":
-                    # K centerlines x batch_size x centerline length x 2
-                    relevant_centerlines_abs = relevant_centerlines.cpu().numpy()
-                elif config.hyperparameters.physical_context == "oracle":
-                    # 1 centerlines x batch_size x centerline length x 2
-                    # relevant_centerlines_abs = phy_info.unsqueeze(0).cpu().numpy() + map_origin.cpu().numpy()
-                    relevant_centerlines_abs = relevant_centerlines.unsqueeze(0).cpu().numpy()
-                else:
+                if config.hyperparameters.physical_context == "dummy" or config.hyperparameters.physical_context == "social":
                     relevant_centerlines_abs = []
-
+                else:
+                    relevant_centerlines_abs = relevant_centerlines.cpu().numpy()
+                    
                 plot_functions.viz_predictions_all(seq_id,
                                                    results_path,
-                                                   obs_traj.permute(1,0,2).cpu().numpy(), # All obstacles
-                                                   pred_traj_fake.squeeze(0).cpu().numpy(), # Only AGENT (MM prediction)
-                                                   pred_traj_gt.permute(1,0,2).cpu().numpy(), # All obstacles
-                                                   curr_object_class_id_list.cpu().numpy(),
+                                                   obs_traj.permute(1,0,2).numpy(), # All obstacles
+                                                   pred_traj_fake.squeeze(0).numpy(), # Only AGENT (MM prediction)
+                                                   pred_traj_gt.permute(1,0,2).numpy(), # All obstacles
+                                                   target_agent_orientation.numpy(),
+                                                   curr_object_class_id_list.numpy(),
                                                    city_name,
-                                                   curr_map_origin.cpu().numpy(),
+                                                   curr_map_origin.numpy(),
                                                    avm,
                                                    dist_rasterized_map=50,
                                                    relevant_centerlines_abs=relevant_centerlines_abs,
-                                                   show=False,
                                                    save=True,
                                                    ade_metric=ade_min,
                                                    fde_metric=fde_min,
@@ -446,7 +468,7 @@ def main(args):
     # Dataloader
 
     print(f"Load {config.dataset.split} split...")
-
+    
     data_split = ArgoverseMotionForecastingDataset(dataset_name=config.dataset_name,
                                                    root_folder=config.dataset.path,
                                                    imgs_folder=config.dataset.imgs_folder,
@@ -455,11 +477,14 @@ def main(args):
                                                    distance_threshold=config.hyperparameters.distance_threshold,
                                                    split=config.dataset.split,
                                                    split_percentage=config.dataset.split_percentage,
-                                                   batch_size=config.dataset.batch_size,
                                                    class_balance=config.dataset.class_balance,
                                                    hard_mining=config.dataset.hard_mining,
                                                    obs_origin=config.hyperparameters.obs_origin,
-                                                   physical_context=config.hyperparameters.physical_context)
+                                                   apply_rotation=config.dataset.apply_rotation,
+                                                   physical_context=config.hyperparameters.physical_context,
+                                                   extra_data_train=config.dataset.extra_data_train,
+                                                   preprocess_data=config.dataset.preprocess_data,
+                                                   save_data=config.dataset.save_data)
 
     split_loader = DataLoader(data_split,
                               batch_size=config.dataset.batch_size,
@@ -534,6 +559,6 @@ python evaluate/argoverse/generate_results_rel-rel.py \
 
 """
 python evaluate/argoverse/generate_results_rel-rel.py \
---model_path "save/argoverse/sophie_mm/100.0_percent/stable_model_test_gnn_mhsa_centerlines_2/argoverse_motion_forecasting_dataset_0_with_model.pt" \
---device_gpu 0 --split "val"
+--model_path "save/argoverse/mapfe4mp/100.0_percent/mapfe4mp_7/argoverse_motion_forecasting_dataset_0_with_model.pt" \
+--device_gpu 0 --split "test"
 """

model/datasets/argoverse/data_augmentation_functions.py
@@ -155,5 +155,5 @@ def rotate_traj(traj,R):
 
     # traj = torch.matmul(traj,R) # (N x 2) x (2 x 2)
     traj = np.matmul(traj,R).float() # (N x 2) x (2 x 2)
-    
+   
     return traj

model/datasets/argoverse/dataset.py
@@ -224,9 +224,9 @@ def seq_collate(data):
             # Get current centerlines
 
             if PHYSICAL_CONTEXT == "plausible_centerlines": # N centerlines
-                curr_relevant_centerlines = phy_info[seq_index,:,:,:].unsqueeze(1) # N x 1 x centerline_length x 2
+                curr_relevant_centerlines = phy_info[seq_index,:,:,:].unsqueeze(0) # 1 (sequence) x N centerlines x centerline_length x 2
             elif PHYSICAL_CONTEXT == "oracle": # Only the most plausible
-                curr_relevant_centerlines = phy_info[seq_index,:,:].unsqueeze(1) # 1 x 1 x centerline_length x 2
+                curr_relevant_centerlines = phy_info[seq_index,:,:].unsqueeze(0) # 1 (sequence) x 1 centerline x centerline_length x 2
             else:
                 curr_relevant_centerlines = torch.tensor([])
                 
@@ -262,13 +262,15 @@ def seq_collate(data):
             aug_curr_pred_traj_gt_rel[1:,:,:] = torch.sub(rotated_curr_pred_traj_gt[1:,:,:],
                                                           rotated_curr_pred_traj_gt[:-1,:,:])
             
-            ## Fill torch tensor (whole batch)
+            ## Update torch tensor (whole batch)
 
             obs_traj[:,start:end,:] = rotated_aug_curr_obs_traj
             obs_traj_rel[:,start:end,:] = aug_curr_obs_traj_rel
             pred_traj_gt[:,start:end,:] = rotated_curr_pred_traj_gt
             pred_traj_gt_rel[:,start:end,:] = aug_curr_pred_traj_gt_rel
-
+            map_origin[seq_index] = rotated_curr_map_origin
+            phy_info[seq_index,:,:,:] = rotated_curr_relevant_centerlines
+            
             if DEBUG_DATA_AUGMENTATION:
                 if "train" in curr_split_hm:
                     results_path = f"data/datasets/argoverse/motion-forecasting/train"
@@ -674,13 +676,14 @@ class ArgoverseMotionForecastingDataset(Dataset):
                 ex_cum_start_idx = [0] + np.cumsum(ex_num_objs_in_seq).tolist()
                 ex_seq_start_end = [(start, end) for start, end in zip(ex_cum_start_idx, ex_cum_start_idx[1:])]
 
-            if self.hard_mining != -1.0: 
+            if self.hard_mining != -1.0 and split_percentage == 1.0: 
                 # In this file, we can see the most difficult sequences of the validation split
                 file_csv = "results/mapfe4mp/100.0_percent/test_oracle_check_rel2absmm/val/metrics_sorted_ade.csv"
                 
                 df = pd.read_csv(file_csv,sep=" ")
                 seq_index = df["Index"][:-2].astype(int) # from 0 to N-1, not num_seq.csv
-                self.hardest_sequences = seq_index[-self.batch_size:].values # If we have batch_size = N, at most we can
+                self.hardest_sequences = seq_index[-2000:].values
+                # self.hardest_sequences = seq_index[-self.batch_size:].values # If we have batch_size = N, at most we can
                 # introduce N hardest val sequences (if hard_mining = 1.0) in the batch
 
                 ## Create torch data

model/datasets/argoverse/plot_functions.py
@@ -113,7 +113,7 @@ def viz_predictions_all(
         input_ (numpy array): Input Trajectory with shape (num_tracks x obs_len x 2)
         output (numpy array): Top-k predicted trajectories, each with shape (num_tracks x pred_len x 2)
         target (numpy array): Ground Truth Trajectory with shape (num_tracks x pred_len x 2)
-        centerlines (numpy array of list of centerlines): Centerlines (Oracle/Top-k) for each trajectory
+        centerlines (numpy array of list of centerlines): Centerlines (Oracle/Top-k) for each trajectory: 1 x N x length x 2
         city_names (numpy array): city names for each trajectory
         show (bool): if True, show
     """
@@ -138,7 +138,7 @@ def viz_predictions_all(
         output = output_abs + map_origin
     target = target_abs + map_origin
 
-    _, _, points_per_centerline, data_dim = relevant_centerlines_abs.shape
+    _, num_centerlines, points_per_centerline, data_dim = relevant_centerlines_abs.shape
     rows,cols,_ = np.where(relevant_centerlines_abs[:,:,:,0] == 0.0) # only sum the origin to non-padded centerlines
     relevant_centerlines = relevant_centerlines_abs + map_origin
     relevant_centerlines[rows,cols,:,:] = np.zeros((points_per_centerline,data_dim))
@@ -229,15 +229,13 @@ def viz_predictions_all(
 
         # Centerlines (Optional)
     
-        num_centerlines = relevant_centerlines.shape[0]
-
         count_rep_centerlines = np.zeros((num_centerlines))
         rep_centerlines = []
 
         # TODO: Prepare this code to deal with batch size != 1
         if len(relevant_centerlines) > 0:
             for id_centerline in range(num_centerlines):
-                centerline = relevant_centerlines[id_centerline,0,:,:] # We assume batch_size = 1 here
+                centerline = relevant_centerlines[0,id_centerline,:,:] # TODO: We assume batch_size = 1 here
 
                 if not np.any(centerline): # Avoid plotting padded centerlines
                     continue
@@ -281,7 +279,7 @@ def viz_predictions_all(
 
             for index_repeated in range(len(count_rep_centerlines)):
                 if count_rep_centerlines[index_repeated] > 0:
-                    centerline = relevant_centerlines[index_repeated,0,:,:]
+                    centerline = relevant_centerlines[0,index_repeated,:,:] # TODO: We assume batch_size = 1 here
 
                     plt.text(
                             centerline[-1, 0] + 1,

model/models/mapfe4mp_6.py
@@ -566,9 +566,9 @@ class TrajectoryGenerator(nn.Module):
         self.centerline_gnn = GNN(h_dim=self.h_dim_physical)
         self.pattn = MultiheadSelfAttention(h_dim=self.h_dim_physical,
                                             num_heads=self.num_attention_heads)
-        # mlp_phy_attn_dims = [self.num_centerlines*self.h_dim_physical, self.h_dim_physical]
-        # self.mlp_phy_attn = make_mlp(mlp_phy_attn_dims,
-        #                              activation_function="Tanh")
+        mlp_phy_attn_dims = [self.num_centerlines*self.h_dim_physical, self.h_dim_physical]
+        self.mlp_phy_attn = make_mlp(mlp_phy_attn_dims,
+                                     activation_function="Tanh")
         
         # Decoder
 

model/trainers/trainer_mapfe4mp.py
@@ -51,7 +51,6 @@ torch.set_float32_matmul_precision("medium")
 current_cuda = None
 absolute_root_folder = None
 
-USE_SCALER = False # Do not use this scaler, first learn how to use it properly
 CHECK_ACCURACY_TRAIN = False
 CHECK_ACCURACY_VAL = True
 MAX_TIME_TO_CHECK_TRAIN = 120 # minutes
@@ -124,7 +123,7 @@ def calculate_mse_gt_loss_multimodal(gt, pred, loss_f, compute_ade=True, compute
 
     return loss_ade/num_modes, loss_fde/num_modes
 
-def calculate_smoothL1_gt_loss_multimodal(gt, pred, loss_f):
+def calculate_smoothL1_gt_loss_multimodal(gt, pred, loss_f, compute_ade=True, compute_fde=True):
     """
     gt: (pred_len, batch_size, data_dim)
     pred: (batch_size, num_modes, pred_len, data_dim)
@@ -134,12 +133,14 @@ def calculate_smoothL1_gt_loss_multimodal(gt, pred, loss_f):
 
     pred = pred.permute(1,2,0,3) # num_modes, pred_len, batch_size, data_dim
 
-    loss_smoothL1 = torch.zeros(1).to(pred)
-
+    loss_smoothL1_ade = torch.zeros(1).to(pred)
+    loss_smoothL1_fde = torch.zeros(1).to(pred)
+    
     for i in range(num_modes):
-        loss_smoothL1 += loss_f(pred[i,:,:,:], gt)
+        if compute_ade: loss_smoothL1_ade += loss_f(pred[i,:,:,:], gt)
+        if compute_fde: loss_smoothL1_fde += loss_f(pred[i][-1].unsqueeze(0), gt[-1].unsqueeze(0))
 
-    return loss_smoothL1/num_modes
+    return loss_smoothL1_ade/num_modes, loss_smoothL1_fde/num_modes
 
 def calculate_mse_gt_loss_unimodal(gt, pred, loss_f, w_loss=None):
     """
@@ -382,7 +383,7 @@ def model_trainer(config, logger):
     if restore_path is not None and os.path.isfile(restore_path):
         logger.info('Restoring from checkpoint {}'.format(restore_path))
         checkpoint = torch.load(restore_path, map_location=current_cuda)
-        
+
         generator.load_state_dict(checkpoint.config_cp['g_best_state'], strict=False)
         optimizer_g.load_state_dict(checkpoint.config_cp['g_optim_state'])
 
@@ -901,16 +902,19 @@ def generator_step(hyperparameters, batch, generator, optimizer_g,
         losses["G_nll_loss"] = loss_nll.item()
 
     elif hyperparameters.loss_type_g == "mse+L1+nll":
-        loss_smoothL1 = calculate_smoothL1_gt_loss_multimodal(pred_traj_gt, pred_traj_fake, loss_f["smoothL1"])
-        _, loss_fde = calculate_mse_gt_loss_multimodal(pred_traj_gt, pred_traj_fake, loss_f["mse"], compute_ade=False)
+        loss_smoothL1_ade, loss_smoothL1_fde = calculate_smoothL1_gt_loss_multimodal(pred_traj_gt, pred_traj_fake, loss_f["smoothL1"])
+        # _, loss_fde = calculate_mse_gt_loss_multimodal(pred_traj_gt, pred_traj_fake, loss_f["mse"], compute_ade=False)
         loss_nll = calculate_nll_loss(pred_traj_gt, pred_traj_fake, loss_f["nll"], conf)
         
-        loss = hyperparameters.loss_smoothL1_weight*loss_smoothL1 + \
-                hyperparameters.loss_fde_weight*loss_fde + \
-                hyperparameters.loss_nll_weight*loss_nll \
-
-        losses["G_smoothL1_loss"] = loss_smoothL1.item()
-        losses["G_mse_fde_loss"] = loss_fde.item()
+        # loss = hyperparameters.loss_smoothL1_weight*loss_smoothL1 + \
+        #         hyperparameters.loss_fde_weight*loss_fde + \
+        #         hyperparameters.loss_nll_weight*loss_nll \
+        loss = hyperparameters.loss_smoothL1_weight*loss_smoothL1_ade + \
+               hyperparameters.loss_smoothL1_weight*loss_smoothL1_fde + \
+               hyperparameters.loss_nll_weight*loss_nll \
+            
+        losses["G_smoothL1_ade_loss"] = loss_smoothL1_ade.item()
+        losses["G_smoothL1_fde_loss"] = loss_smoothL1_fde.item()
         losses["G_nll_loss"] = loss_nll.item()
         
     elif hyperparameters.loss_type_g == "centerlines+gt":
@@ -939,13 +943,8 @@ def generator_step(hyperparameters, batch, generator, optimizer_g,
             generator.parameters(), hyperparameters.clipping_threshold_g
         )
 
-    if USE_SCALER:
-        scaler.scale(loss).backward()
-        scaler.step(optimizer_g)
-        scaler.update()
-    else:
-        loss.backward()
-        optimizer_g.step()
+    loss.backward()
+    optimizer_g.step()
 
     return losses
 